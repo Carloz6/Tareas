3847e8f4bd518a3f18fb53ca20c5f54458e54a8a
Carlos Z <carlos-zepeda@outlook.com>
2018-11-29 22:26:12 -0600

    Added get tarea through id

--- routes/api.js
+++ routes/api.js
@@ -5,18 +5,27 @@ const db = mongojs('mongodb://Carlos:Newton1@ds121624.mlab.com:21624/magma_tasks
 
 //Get all tasks
 router.get('/tareas', (req, res, next) => {
-    db.tasks.find((err, tasks) => {
-        if(err){
-            res.send(err);
-        }
-        res.json(tasks);
+    db.tasks.find({},(err, tasks) => {
+        err ? res.send(err) : res.json(tasks);
     });
 });
 
 //Get single task
 router.get('/tarea/:id', (req, res, next)=>{
-
+    const getId = mongojs.ObjectId(req.params.id);
+    console.log(getId);
+    db.tasks.find({_id: getId}, (err, task) => {
+        err ? res.send(err) : res.json(task);
+    });
 });
 
+//delete
+/*
+router.get('/borrar/:id', (req, res, next)=>{
+    const getId = mongojs.ObjectId(req.params.id);
+    db.tasks.delete({_id: getId}, (err, task) => {
+        err ? res.send(err) : res.json(task);
+    });
+});*/
 
 module.exports = router;
